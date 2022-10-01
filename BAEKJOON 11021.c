410da95796668bea30de1d4a8ff3a73e73b3d4c3
김영준 <102145868+kimyj0530@users.noreply.github.com>
2022-10-02 02:16:08 +0900

    Create BAEKJOON 11021.c

---
+++ BAEKJOON 11021.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+int main(void) {
+  int a;
+  int n;
+  scanf("%d", &n);
+  for (int i = 1; i <= n; i++) {
+    int b;
+    scanf("%d %d", &a, &b);
+    printf("Case #%d: %d\n", i, a + b);
+  }
+  return 0;
+}
