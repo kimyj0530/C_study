540af939fb5d13748ab0545a809a0917eb3a60f1
김영준 <102145868+kimyj0530@users.noreply.github.com>
2022-10-05 19:23:01 +0900

    Create BAEKJOON 2439.c

---
+++ BAEKJOON 2439.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+int main(void) {
+  int n;
+  scanf("%d", &n);
+  for (int i = 1; i <= n; i++) {
+    for (int j = i; j <= n - 1; j++) {
+      printf(" ");
+    }
+    for (int k = 1; k <= i; k++) {
+      printf("*");
+    }
+
+    printf("\n");
+  }
+  return 0;
+}
