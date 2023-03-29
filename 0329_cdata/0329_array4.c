6a79e5118857be02adcfc30a9f5cd0ceb195633c
김영준 <102145868+kimyj0530@users.noreply.github.com>
2023-03-29 18:09:20 +0900

    Add files via upload

---
+++ 0329_cdata/0329_array4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+    int arr[2][2] = {
+        {1, 2},
+        {3, 4}
+    };
+    for(int i=0; i<2; i++){
+        for(int j=1; j>=0; j--){
+            printf("%d", arr[i][j]);
+        }
+    }
+    printf("\n");
+    return 0;
+}
