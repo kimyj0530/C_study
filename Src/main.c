1282322f113ae838845a89bdca96c7417b39a6ef
김영준 <102145868+kimyj0530@users.noreply.github.com>
2023-04-13 23:52:18 +0900

    Add files via upload

--- Src/main.c
+++ Src/main.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
-void Delay_Timer(uint32_t time) {
-	for(; time > 0; time--){
-	}
-}
-
 int main(void){
-	RCC->APB2ENR |= (1<<2); //0x4; //PortA 버스 탑승권 허용 
-	GPIOA->CRL &= ~(1<<22); //~0x400000;
-	GPIOA->CRL |= (1<<20);//0x44144444; //PortA PA5 출력 설정
-
+	RCC->APB2ENR = (0x1<<2)|(0x1<<4);
+	GPIOA->CRL &= ~(0x00400000); // (0x1<<22);
+	GPIOA->CRL |= 0x00100000; // (0x1<<20); output select
+	GPIOC->CRH &= ~(0x00400000); // (0x1<<22);
+	GPIOC->CRH |= 0x00800000; // (0x1<<23); input select
+	GPIOC->ODR |= 0x00002000; // (0x1<<13);
+	//GPIOC->IDR |= 0x00002000; // (0x1<<13);
 	while(1){
-		GPIOA->BSRR |= (1<<5); //0x20; // LED ON
-		Delay_Timer(500000);
-		GPIOA->BSRR |= (1<<21); //~0x20; // LED OFF
-		Delay_Timer(500000);
+		if(GPIOC->IDR & 0x000002000){
+				GPIOA->BSRR |= 0x1<<21; // 0x00200000; // LED reset
+		} else{
+			GPIOA-> BSRR |= 0x1<<5; // 0x00000020; //LED set
+		}
 	}
+	return 0;
 }
